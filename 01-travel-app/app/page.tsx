3ca055442e442a055e213fbafa2b933d563afcb4
yaroprof <crosskyiv@gmail.com>
2024-05-30 21:42:52 +0300

    nnm

---
+++ 01-travel-app/app/page.tsx
@@ -0,0 +1,16 @@
+import Camp from "@/components/Camp";
+import Features from "@/components/Features";
+import GetApp from "@/components/GetApp";
+import Hero from "@/components/Hero";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Camp />
+      <Features />
+      <GetApp />
+    </>
+  )
+}
+ 
